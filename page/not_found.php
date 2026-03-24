6dbea0c118e9380a0e2f6e7d7c47339710f9e02a
CapKarel <cap.karel@zak.spspb.cz>
2026-03-24 12:27:15 +0100

    Add custom 404 error page content

--- page/not_found.php
+++ page/not_found.php
@@ -1,1 +1,5 @@
-
+<div class="error-page">
+    <h1>404 – Stránka nenalezena</h1>
+    <p>Omlouváme se, ale tato sekce neexistuje.</p>
+    <a href="index.php?page=home">Zpět na hlavní stránku</a>
+</div>
